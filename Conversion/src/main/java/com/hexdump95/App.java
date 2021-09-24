14d5207c3461f546efd596b732a60101482da51d
Sergio Villanueva <65696419+hexdump95@users.noreply.github.com>
2021-09-23 22:41:14 -0300

    Add function (#2)
    * Add conversion handler
    
    * Add template.yaml

--- Conversion/src/main/java/com/hexdump95/App.java
+++ Conversion/src/main/java/com/hexdump95/App.java
@@ -1,10 +1,40 @@
 package com.hexdump95;
 
-/**
- * Hello world!
- */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+
+        String conversionValue = input.getPathParameters().get("decimal");
+
+        if (!conversionValue.matches("([0-9]+)$"))
+            return response.withStatusCode(400);
+
+        int decimal = Integer.parseInt(conversionValue);
+
+        String toBinary = Integer.toBinaryString(decimal);
+        String toOctal = Integer.toOctalString(decimal);
+        String toHexadecimal = Integer.toHexString(decimal);
+        String output = String.format("{ " +
+                "\"binary\": \"%s\", " +
+                "\"octal\": \"%s\", " +
+                "\"hexadecimal\": \"%s\" }", toBinary, toOctal, toHexadecimal);
+
+        return response
+                .withStatusCode(200)
+                .withBody(output);
     }
+
 }
